bbfbbf99071d4e9f07b0866b1ded9dff0b7f903d
as30098 <alexspencer421@gmail.com>
2021-01-12 13:51:04 -0500

    completed deliverables 1 - 3

--- index.js
+++ index.js
@@ -4,15 +4,43 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+let h1 = document.querySelector('h1#header')
+h1.style.color = "green"
 
 
 /***** Deliverable 3 *****/
-// console.log('This is what the traveler object looks like: ', traveler)
+console.log('This is what the traveler object looks like: ', traveler)
+
+
+  console.log(traveler.name)
+  console.log(traveler.nickname)
+  console.log(traveler.photo)
+
+  let name = document.querySelector('h2')
+  name.textContent = traveler.name
+  let nickname = document.querySelector('em')
+  nickname.textContent = traveler.nickname
+  let photoSrc = document.querySelector('img').setAttribute('src', traveler.photo)
+  let photoAlt = document.querySelector('img').setAttribute('alt', 'Raffy')
+
+
 
 
 /***** Deliverable 4 *****/
+  // traveler.animalSightings.forEach(
+  //   // create elements
+  //   document.createElement('li')
+  //   document.createElement('p')
+  //   document.createElement('img')
+  //   document.createElement('a')
+
+  //   // add attributes
+
+  //   // relationships
+
+  //   // show on DOM
 
 
+  // )
 
 /***** Deliverable 5 *****/
